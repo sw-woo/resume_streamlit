f6592bcbd6ec4d1cbc70bb8ac3fd0480635ca4e0
sungwoo <62142688+sw-woo@users.noreply.github.com>
2025-01-04 16:47:20 +0900

    Create resume_streamlit.py
    로또번호 생성기

---
+++ resume_streamlit.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import random
+import datetime
+
+st.title(':sparkles:로또 생성기:sparkles:')
+
+
+def generate_lotto():
+    lotto = set()
+
+    while len(lotto) < 6:
+        number = random.randint(1, 46)
+        lotto.add(number)
+
+    lotto = list(lotto)
+    lotto.sort()
+    return lotto
+
+# st.subheader(f'행운의 번호: :green[{generate_lotto()}]')
+# st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
+
+button = st.button('로또를 생성해 주세요!')
+
+number = st.text_input(
+    label='로또 자동 게임 횟수를 입력해주세요', 
+    placeholder='숫자로 입력해주세요'
+    )
+
+if button:
+    if number == '' or number == '0' or number.isnumeric() == False:
+        st.write('로또 자동 게임 횟수를 입력해주세요')
+    else:
+        for i in range(1, int(number)+1):
+            st.subheader(f'{i}. 행운의 번호: :green[{generate_lotto()}]')
+    st.write(f"생성된 시각: {datetime.datetime.now().strftime('%Y-%m-%d %H:%M')}")
